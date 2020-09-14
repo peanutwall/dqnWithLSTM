d4ffcfa7615371062d7e21f4e5add283224e0326
peanutwall <57250847+peanutwall@users.noreply.github.com>
2020-09-14 21:21:12 +0800

    Add files via upload

--- LSTM/src/buildModelLSTM.py
+++ LSTM/src/buildModelLSTM.py
@@ -6,12 +6,136 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+# This is alternative
+# class BuildSimpleModel:
+#     def __init__(self, numStateSpace, numActionSpace, gamma, seed=1):
+#         self.numStateSpace = numStateSpace
+#         self.numActionSpace = numActionSpace
+#         self.gamma = gamma
+#         self.seed = seed
+#
+#     def __call__(self, sigmoidLayersWidths, tanhLayerWidths, summaryPath="./tbdata"):
+#         print("Generating LSTM Model with layers: {}, {}".format(sigmoidLayersWidths, tanhLayerWidths))
+#         graph = tf.Graph()
+#         with graph.as_default():
+#             if self.seed is not None:
+#                 tf.set_random_seed(self.seed)
+#
+#             with tf.name_scope('inputs'):
+#                 states_ = tf.placeholder(tf.float32, [None, self.numStateSpace], name="states")
+#                 act_ = tf.placeholder(tf.float32, [None, self.numActionSpace], name="act")
+#                 formerOutput_ = tf.placeholder(tf.float32, [None, self.numActionSpace], name="formerOutput")
+#                 formerCell_ = tf.placeholder(tf.float32, [None, self.numActionSpace], name="formerCell")
+#                 yi_ = tf.placeholder(tf.float32, [None, 1], name="yi")
+#                 tf.add_to_collection("states", states_)
+#                 tf.add_to_collection("act", act_)
+#                 tf.add_to_collection("yi", yi_)
+#                 tf.add_to_collection("formerCell", formerCell_)
+#                 tf.add_to_collection("formerOutput", formerOutput_)
+#
+#             inputStates_ = tf.concat([formerOutput_, states_], 1)
+#             initWeight = tf.random_uniform_initializer(-0.03, 0.03)
+#             initBias = tf.constant_initializer(0.01)
+#
+#             with tf.variable_scope("sigmoidGate"):
+#                 with tf.variable_scope("trainHiddenSigmoid"):
+#                     activation_ = inputStates_
+#                     for i in range(len(sigmoidLayersWidths)):
+#                         sigmoidHiddenLayer = tf.layers.Dense(units=sigmoidLayersWidths[i], activation=None,
+#                                                   kernel_initializer=initWeight,
+#                                                   bias_initializer=initBias, name="hiddenSigmoid{}".format(i + 1),
+#                                                   trainable=True)
+#                         activation_ = sigmoidHiddenLayer(activation_)
+#
+#                         tf.add_to_collections(["weights", f"weight/{sigmoidHiddenLayer.kernel.name}"], sigmoidHiddenLayer.kernel)
+#                         tf.add_to_collections(["biases", f"bias/{sigmoidHiddenLayer.bias.name}"], sigmoidHiddenLayer.bias)
+#                         tf.add_to_collections(["activations", f"activation/{activation_.name}"], activation_)
+#                     sigmoidHiddenOutput_ = tf.identity(activation_, name="sigmoidHiddenOutput")
+#                     sigmoidOutputLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.sigmoid,
+#                                                         kernel_initializer=initWeight,
+#                                                         bias_initializer=initBias,
+#                                                         name="outputSigmoid{}".format(len(sigmoidLayersWidths) + 1),
+#                                                         trainable=True)
+#                     sigmoidOutput_ = sigmoidOutputLayer(sigmoidHiddenOutput_)
+#                     tf.add_to_collections(["weights", f"weight/{sigmoidOutputLayer.kernel.name}"], sigmoidOutputLayer.kernel)
+#                     tf.add_to_collections(["biases", f"bias/{sigmoidOutputLayer.bias.name}"], sigmoidOutputLayer.bias)
+#                     tf.add_to_collections("sigmoidOutput", sigmoidOutput_)
+#
+#             with tf.variable_scope("tanhGate"):
+#                 with tf.variable_scope("trainHiddenTanh"):
+#                     activation_ = inputStates_
+#                     for i in range(len(tanhLayerWidths)):
+#                         tanhHiddenLayer = tf.layers.Dense(units=tanhLayerWidths[i], activation=None,
+#                                                   kernel_initializer=initWeight,
+#                                                   bias_initializer=initBias, name="hiddenTanh{}".format(i + 1),
+#                                                   trainable=True)
+#                         activation_ = tanhHiddenLayer(activation_)
+#
+#                         tf.add_to_collections(["weights", f"weight/{tanhHiddenLayer.kernel.name}"], tanhHiddenLayer.kernel)
+#                         tf.add_to_collections(["biases", f"bias/{tanhHiddenLayer.bias.name}"], tanhHiddenLayer.bias)
+#                         tf.add_to_collections(["activations", f"activation/{activation_.name}"], activation_)
+#                     tanhHiddenOutput_ = tf.identity(activation_, name="tanhHiddenOutput")
+#                     outputTanhLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.tanh,
+#                                                         kernel_initializer=initWeight,
+#                                                         bias_initializer=initBias,
+#                                                         name="outputTanh{}".format(len(tanhLayerWidths) + 1),
+#                                                         trainable=True)
+#                     tanhOutput_ = outputTanhLayer(tanhHiddenOutput_)
+#                     tf.add_to_collections(["weights", f"weight/{outputTanhLayer.kernel.name}"], outputTanhLayer.kernel)
+#                     tf.add_to_collections(["biases", f"bias/{outputTanhLayer.bias.name}"], outputTanhLayer.bias)
+#                     tf.add_to_collections("tanhOutput", tanhOutput_)
+#
+#             with tf.variable_scope("trainingParams"):
+#                 learningRate_ = tf.constant(0.001, dtype=tf.float32)
+#                 tf.add_to_collection("learningRate", learningRate_)
+#
+#             with tf.variable_scope("cell"):
+#                 outputCell_ = sigmoidOutput_*formerCell_+sigmoidOutput_*tanhOutput_
+#                 tf.add_to_collection("outputCell", outputCell_)
+#
+#             with tf.variable_scope("output"):
+#                 output_ = tf.tanh(outputCell_)*sigmoidOutput_
+#                 tf.add_to_collection("output", output_)
+#
+#             with tf.variable_scope("QTable"):
+#                 QEval_ = tf.reduce_sum(tf.multiply(output_, act_), reduction_indices=1)
+#                 tf.add_to_collections("QEval", QEval_)
+#                 QEval_ = tf.reshape(QEval_, [-1, 1])
+#                 # loss_ = tf.reduce_mean(tf.square(yi_ - QEval_))
+#                 loss_ = tf.reduce_mean(tf.square(yi_ - QEval_))
+#                 # loss_ = tf.losses.mean_squared_error(labels=yi_, predictions=QEval_)
+#                 tf.add_to_collection("loss", loss_)
+#
+#             with tf.variable_scope("train"):
+#                 trainOpt_ = tf.train.AdamOptimizer(learningRate_, name='adamOptimizer').minimize(loss_)
+#                 tf.add_to_collection("trainOp", trainOpt_)
+#
+#                 saver = tf.train.Saver(max_to_keep=None)
+#                 tf.add_to_collection("saver", saver)
+#
+#             fullSummary = tf.summary.merge_all()
+#             tf.add_to_collection("summaryOps", fullSummary)
+#             if summaryPath is not None:
+#                 trainWriter = tf.summary.FileWriter(summaryPath + "/train", graph=tf.get_default_graph())
+#                 testWriter = tf.summary.FileWriter(summaryPath + "/test", graph=tf.get_default_graph())
+#                 tf.add_to_collection("writers", trainWriter)
+#                 tf.add_to_collection("writers", testWriter)
+#             saver = tf.train.Saver(max_to_keep=None)
+#             tf.add_to_collection("saver", saver)
+#
+#             # self.soft_replace = [tf.assign(t, (1 - self.TAU) * t + self.TAU * e)
+#             #         for t, e in zip(self.at_params + self.ct_params, self.ae_params + self.ce_params)]
+#
+#             model = tf.Session(graph=graph)
+#             model.run(tf.global_variables_initializer())
+#
+#         return model
+
 
 class BuildModel:
-    def __init__(self, numStateSpace, numActionSpace, gamma, seed=1):
+    def __init__(self, numStateSpace, numActionSpace, seed=1):
         self.numStateSpace = numStateSpace
         self.numActionSpace = numActionSpace
-        self.gamma = gamma
         self.seed = seed
 
     def __call__(self, sigmoidLayersWidths, tanhLayerWidths, summaryPath="./tbdata"):
@@ -37,29 +161,53 @@ class BuildModel:
             initWeight = tf.random_uniform_initializer(-0.03, 0.03)
             initBias = tf.constant_initializer(0.01)
 
-            with tf.variable_scope("sigmoidGate"):
-                with tf.variable_scope("trainHiddenSigmoid"):
+            with tf.variable_scope("forgetSigmoidGate"):
+                with tf.variable_scope("trainForgetHidden"):
                     activation_ = inputStates_
                     for i in range(len(sigmoidLayersWidths)):
-                        sigmoidHiddenLayer = tf.layers.Dense(units=sigmoidLayersWidths[i], activation=None,
+                        forgetHiddenLayer = tf.layers.Dense(units=sigmoidLayersWidths[i], activation=None,
                                                   kernel_initializer=initWeight,
-                                                  bias_initializer=initBias, name="hiddenSigmoid{}".format(i + 1),
+                                                  bias_initializer=initBias, name="forgetHidden{}".format(i + 1),
                                                   trainable=True)
-                        activation_ = sigmoidHiddenLayer(activation_)
+                        activation_ = forgetHiddenLayer(activation_)
 
-                        tf.add_to_collections(["weights", f"weight/{sigmoidHiddenLayer.kernel.name}"], sigmoidHiddenLayer.kernel)
-                        tf.add_to_collections(["biases", f"bias/{sigmoidHiddenLayer.bias.name}"], sigmoidHiddenLayer.bias)
+                        tf.add_to_collections(["weights", f"weight/{forgetHiddenLayer.kernel.name}"], forgetHiddenLayer.kernel)
+                        tf.add_to_collections(["biases", f"bias/{forgetHiddenLayer.bias.name}"], forgetHiddenLayer.bias)
                         tf.add_to_collections(["activations", f"activation/{activation_.name}"], activation_)
-                    sigmoidHiddenOutput_ = tf.identity(activation_, name="sigmoidHiddenOutput")
-                    sigmoidOutputLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.sigmoid,
+                    forgetHiddenOutput_ = tf.identity(activation_, name="forgetHiddenOutput")
+                    forgetOutputLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.sigmoid,
                                                         kernel_initializer=initWeight,
                                                         bias_initializer=initBias,
-                                                        name="outputSigmoid{}".format(len(sigmoidLayersWidths) + 1),
+                                                        name="forgetOutputLayer{}".format(len(sigmoidLayersWidths) + 1),
                                                         trainable=True)
-                    sigmoidOutput_ = sigmoidOutputLayer(sigmoidHiddenOutput_)
-                    tf.add_to_collections(["weights", f"weight/{sigmoidOutputLayer.kernel.name}"], sigmoidOutputLayer.kernel)
-                    tf.add_to_collections(["biases", f"bias/{sigmoidOutputLayer.bias.name}"], sigmoidOutputLayer.bias)
-                    tf.add_to_collections("sigmoidOutput", sigmoidOutput_)
+                    forgetOutput_ = forgetOutputLayer(forgetHiddenOutput_)
+                    tf.add_to_collections(["weights", f"weight/{forgetOutputLayer.kernel.name}"], forgetOutputLayer.kernel)
+                    tf.add_to_collections(["biases", f"bias/{forgetOutputLayer.bias.name}"], forgetOutputLayer.bias)
+                    tf.add_to_collections("forgetOutput", forgetOutput_)
+
+            with tf.variable_scope("inputSigmoidGate"):
+                with tf.variable_scope("trainInputHidden"):
+                    activation_ = inputStates_
+                    for i in range(len(sigmoidLayersWidths)):
+                        inputHiddenLayer = tf.layers.Dense(units=sigmoidLayersWidths[i], activation=None,
+                                                  kernel_initializer=initWeight,
+                                                  bias_initializer=initBias, name="inputHidden{}".format(i + 1),
+                                                  trainable=True)
+                        activation_ = inputHiddenLayer(activation_)
+
+                        tf.add_to_collections(["weights", f"weight/{inputHiddenLayer.kernel.name}"], inputHiddenLayer.kernel)
+                        tf.add_to_collections(["biases", f"bias/{inputHiddenLayer.bias.name}"], inputHiddenLayer.bias)
+                        tf.add_to_collections(["activations", f"activation/{activation_.name}"], activation_)
+                    inputHiddenOutput_ = tf.identity(activation_, name="inputHiddenOutput")
+                    inputOutputLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.sigmoid,
+                                                        kernel_initializer=initWeight,
+                                                        bias_initializer=initBias,
+                                                        name="forgetOutputLayer{}".format(len(sigmoidLayersWidths) + 1),
+                                                        trainable=True)
+                    inputOutput_ = inputOutputLayer(inputHiddenOutput_)
+                    tf.add_to_collections(["weights", f"weight/{inputOutputLayer.kernel.name}"], inputOutputLayer.kernel)
+                    tf.add_to_collections(["biases", f"bias/{inputOutputLayer.bias.name}"], inputOutputLayer.bias)
+                    tf.add_to_collections("inputOutput", inputOutput_)
 
             with tf.variable_scope("tanhGate"):
                 with tf.variable_scope("trainHiddenTanh"):
@@ -85,16 +233,40 @@ class BuildModel:
                     tf.add_to_collections(["biases", f"bias/{outputTanhLayer.bias.name}"], outputTanhLayer.bias)
                     tf.add_to_collections("tanhOutput", tanhOutput_)
 
+            with tf.variable_scope("opSigmoidGate"):
+                with tf.variable_scope("trainOpHidden"):
+                    activation_ = inputStates_
+                    for i in range(len(sigmoidLayersWidths)):
+                        opHiddenLayer = tf.layers.Dense(units=sigmoidLayersWidths[i], activation=None,
+                                                  kernel_initializer=initWeight,
+                                                  bias_initializer=initBias, name="opHidden{}".format(i + 1),
+                                                  trainable=True)
+                        activation_ = opHiddenLayer(activation_)
+
+                        tf.add_to_collections(["weights", f"weight/{opHiddenLayer.kernel.name}"], opHiddenLayer.kernel)
+                        tf.add_to_collections(["biases", f"bias/{opHiddenLayer.bias.name}"], opHiddenLayer.bias)
+                        tf.add_to_collections(["activations", f"activation/{activation_.name}"], activation_)
+                    opHiddenOutput_ = tf.identity(activation_, name="opHiddenOutput")
+                    opOutputLayer = tf.layers.Dense(units=self.numActionSpace, activation=tf.sigmoid,
+                                                        kernel_initializer=initWeight,
+                                                        bias_initializer=initBias,
+                                                        name="opOutputLayer{}".format(len(sigmoidLayersWidths) + 1),
+                                                        trainable=True)
+                    opOutput_ = opOutputLayer(opHiddenOutput_)
+                    tf.add_to_collections(["weights", f"weight/{opOutputLayer.kernel.name}"], opOutputLayer.kernel)
+                    tf.add_to_collections(["biases", f"bias/{opOutputLayer.bias.name}"], opOutputLayer.bias)
+                    tf.add_to_collections("opOutput", opOutput_)
+
             with tf.variable_scope("trainingParams"):
                 learningRate_ = tf.constant(0.001, dtype=tf.float32)
                 tf.add_to_collection("learningRate", learningRate_)
 
             with tf.variable_scope("cell"):
-                outputCell_ = sigmoidOutput_*formerCell_+sigmoidOutput_*tanhOutput_
+                outputCell_ = forgetOutput_*formerCell_+inputOutput_*tanhOutput_
                 tf.add_to_collection("outputCell", outputCell_)
 
             with tf.variable_scope("output"):
-                output_ = tf.tanh(outputCell_)*sigmoidOutput_
+                output_ = tf.tanh(outputCell_)*opOutput_
                 tf.add_to_collection("output", output_)
 
             with tf.variable_scope("QTable"):
